40155938c3ecf8168307c56049c21d4d8fbd4d3d
Steven Gebhardt <stevengebhardt@stevens-mbp.lan>
2023-07-24 16:52:51 -0400

    local storage saving and reading but it has removed the complete and remove buttons

--- Excersises/To-Dos/app.js
+++ Excersises/To-Dos/app.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
     newToDo.appendChild(removeButton);
     list.appendChild(newToDo);
 
-    toDoForm.reset();
+    // toDoForm.reset();
   });
 
   list.addEventListener("click", function (e) {
@@ -30,6 +30,50 @@ document.addEventListener("DOMContentLoaded", function () {
       e.target.parentNode.style.textDecoration = "line-through";
     } else if (target === "remove") {
       e.target.parentNode.remove();
+    }
+  });
+
+  // Start of local storage portion
+  //pull info from local storage
+  const savedItems = JSON.parse(localStorage.getItem("toDo")) || [];
+  for (let i = 0; i < savedItems.length; i++) {
+    let newToDo = document.createElement("li");
+    newToDo.innerText = savedItems[i].toDo;
+    newToDo.isCompleted = savedItems[i].isCompleted ? true : false;
+    if (newToDo.isCompleted) {
+      newToDo.style.textDecoration = "line-through";
+    }
+    list.appendChild(newToDo);
+  }
+
+  toDoForm.addEventListener("click", function (e) {
+    e.preventDefault();
+    let newToDo = document.createElement("li");
+    let task = document.getElementById("toDo").value;
+    newToDo.innerText = task;
+    newToDo.isCompleted = false;
+    toDoForm.reset();
+    list.appendChild(newToDo);
+
+    savedItems.push({ toDo: newToDo.innerText, isCompleted: false });
+    localStorage.setItem("toDo", JSON.stringify(savedItems));
+  });
+
+  list.addEventListener("click", function (e) {
+    let clickedItem = e.target;
+    if (!clickedItem.isCompleted) {
+      clickedItem.style.textDecoration = "line-through";
+      clickedItem.isCompleted = true;
+    } else {
+      clickedItem.style.textDecoration = "none";
+      clickedItem.isCompleted = false;
+    }
+
+    for (let i = 0; i < savedTodos.length; i++) {
+      if (savedTodos[i].task === clickedItem.innerText) {
+        savedTodos[i].isCompleted = !savedTodos[i].isCompleted;
+        localStorage.setItem("todos", JSON.stringify(savedTodos));
+      }
     }
   });
 });
